3c855a5c7f5d07786b9c8adb0ca2058274a73055
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-03-22 14:13:42 +0900

    innit commit

---
+++ src/main/java/com/example/devsreviewproject/DevsReviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.devsreviewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsReviewProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevsReviewProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devsreviewproject/DevsReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devsreviewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsReviewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
