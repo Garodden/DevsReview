62476cad268f61f2a325407f3c9dc72ea8c23963
Hanbit <ghiesf@naver.com>
2024-03-26 16:46:11 +0900

    Add: User 리포지토리, 서비스 추가

--- src/main/java/com/example/KeyboardArenaProject/repository/UserRepository.java
+++ src/main/java/com/example/KeyboardArenaProject/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.KeyboardArenaProject.repository;
 
-public interface UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.KeyboardArenaProject.entity.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+	Optional<User> findByUserId(String id); // id로 사용자 정보를 가져옴
 }
--- src/main/java/com/example/KeyboardArenaProject/service/user/UserService.java
+++ src/main/java/com/example/KeyboardArenaProject/service/user/UserService.java
@@ -1,4 +1,23 @@
 package com.example.KeyboardArenaProject.service.user;
 
-public class UserService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.KeyboardArenaProject.repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+	private UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+		return userRepository.findByUserId(id).orElseThrow(() -> new IllegalArgumentException(id));
+	}
+
 }
