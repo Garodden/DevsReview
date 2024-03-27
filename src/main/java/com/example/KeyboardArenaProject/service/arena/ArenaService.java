7de19cbe2f5788a613438ab072febdc63131ce30
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-03-27 10:37:30 +0900

    Feat: 아레나 서비스에서 주간 랭킹 아레나만 가져오는 메소드 추가

--- src/main/java/com/example/KeyboardArenaProject/service/arena/ArenaService.java
+++ src/main/java/com/example/KeyboardArenaProject/service/arena/ArenaService.java
@@ -1,4 +1,21 @@
 package com.example.KeyboardArenaProject.service.arena;
 
+import com.example.KeyboardArenaProject.entity.BoardEntity;
+import com.example.KeyboardArenaProject.repository.BoardRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ArenaService {
+    private BoardRepository boardRepository;
+
+    public ArenaService(BoardRepository boardRepository){
+        this.boardRepository = boardRepository;
+    }
+
+    public List<BoardEntity> findAllRankArena(){
+        return boardRepository.findAllByBoardType(2);
+    }
+
 }
