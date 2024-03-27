2ebace0afb89cc28a01a612a648c36d49fcc890d
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-03-27 15:20:39 +0900

    fixed: board entity namd feat: 아레나의 리스트를 보여주는 컨트롤러 추가.

--- src/main/java/com/example/KeyboardArenaProject/controller/arena/ArenaController.java
+++ src/main/java/com/example/KeyboardArenaProject/controller/arena/ArenaController.java
@@ -1,4 +1,38 @@
 package com.example.KeyboardArenaProject.controller.arena;
 
+import com.example.KeyboardArenaProject.dto.arena.ArenaResponse;
+import com.example.KeyboardArenaProject.entity.Board;
+import com.example.KeyboardArenaProject.service.arena.ArenaService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "아레나 CRUD")
+@RestController
 public class ArenaController {
+    private final ArenaService arenaService;
+    public ArenaController(ArenaService arenaService){ this.arenaService = arenaService;}
+    @GetMapping("/arenas")
+    public List<ArenaResponse> showArena() {
+
+        List<Board> arenaList = arenaService.findAllRankArena();
+        arenaList.addAll(arenaService.findTop3ArenaOrderByLikes());
+        arenaList.addAll(arenaService.findNormalArenaOrderByCreatedDate());
+        
+        List<ArenaResponse> ArenaResponseList = arenaList.stream()
+                .map(ArenaResponse::new)
+                .toList();
+
+        return ArenaResponseList;
+        
+    }
+
+
+
 }
+
+
