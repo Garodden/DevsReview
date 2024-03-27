fac86f22c119c634707ea32a86a87022a556fb51
YH <jsm1252@gmail.com>
2024-03-27 17:39:03 +0900

    Feat: MyPageEntity 삭제, MyPageRepository, MyPageService 수정

--- src/main/java/com/example/KeyboardArenaProject/repository/MyPageRepository.java
+++ src/main/java/com/example/KeyboardArenaProject/repository/MyPageRepository.java
@@ -1,7 +1,7 @@
 package com.example.KeyboardArenaProject.repository;
 
-import com.example.KeyboardArenaProject.entity.MyPageEntity;
+import com.example.KeyboardArenaProject.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MyPageRepository extends JpaRepository<MyPageEntity, String> {
+public interface MyPageRepository extends JpaRepository<UserEntity, String> {
 }
--- src/main/java/com/example/KeyboardArenaProject/service/user/MyPageService.java
+++ src/main/java/com/example/KeyboardArenaProject/service/user/MyPageService.java
@@ -1,8 +1,6 @@
 package com.example.KeyboardArenaProject.service.user;
 
 import com.example.KeyboardArenaProject.repository.MyPageRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
