51040ad2466686c556ccd886d812c23f58ba1c1f
Hanbit <ghiesf@naver.com>
2024-03-27 21:58:26 +0900

    Feat: 로그아웃 기능 구현

--- src/main/java/com/example/KeyboardArenaProject/controller/user/UserController.java
+++ src/main/java/com/example/KeyboardArenaProject/controller/user/UserController.java
@@ -2,13 +2,19 @@ package com.example.KeyboardArenaProject.controller.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.example.KeyboardArenaProject.dto.user.AddUserRequest;
 import com.example.KeyboardArenaProject.dto.user.UserResponse;
 import com.example.KeyboardArenaProject.entity.User;
 import com.example.KeyboardArenaProject.service.user.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserController {
@@ -23,5 +29,12 @@ public class UserController {
 		User user = userService.save(request);
 		return ResponseEntity.status(HttpStatus.CREATED)
 			.body(user.toResponse());
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		new SecurityContextLogoutHandler().logout(request, response,
+			SecurityContextHolder.getContext().getAuthentication());
+		return "redirect:/login";
 	}
 }
