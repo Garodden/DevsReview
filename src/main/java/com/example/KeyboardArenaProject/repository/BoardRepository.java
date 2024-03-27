e062881068cd702a3f9e76e225e309ad77824f31
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-03-27 10:38:32 +0900

    Feat: 아레나 리포지토리에서 필요한 JPA 쿼리 메소드 추가

--- src/main/java/com/example/KeyboardArenaProject/repository/BoardRepository.java
+++ src/main/java/com/example/KeyboardArenaProject/repository/BoardRepository.java
@@ -4,7 +4,10 @@ import com.example.KeyboardArenaProject.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, String> {
 
+    List<BoardEntity> findAllByBoardType(int i);
 }
