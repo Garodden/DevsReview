2b45c75ca2c4307478171f3d95329cce341ba029
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-03-27 13:33:04 +0900

    Feat: BoardRepository  -findTop3ArenaByOrderByLikeDesc():일반 아레나에서 좋아요를 가장 많이 받은 아레나 3개  -findRestArenaOrderByCreatedDateDesc()나머지 일반 아레나를 작성 날짜 기준으로 내림차순 정렬 ArenaService -findTop3ArenaOrderByLikes():findTop3ArenaByOrderByLikeDesc()함수 호출 -findRestArenaOrderByDate:  findRestArenaOrderByCreatedDateDesc() 함수 호출

--- src/main/java/com/example/KeyboardArenaProject/repository/BoardRepository.java
+++ src/main/java/com/example/KeyboardArenaProject/repository/BoardRepository.java
@@ -2,6 +2,7 @@ package com.example.KeyboardArenaProject.repository;
 
 import com.example.KeyboardArenaProject.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,12 @@ import java.util.List;
 public interface BoardRepository extends JpaRepository<BoardEntity, String> {
 
     List<BoardEntity> findAllByBoardType(int i);
+
+    // top 3 liked normal arena
+    @Query("Select b FROM BoardEntity b where b.boardType = 3 order by b.like desc")
+    List<BoardEntity> findTop3ArenaByOrderByLikeDesc();
+
+    // rest of the normal arena
+    @Query("Select a from BoardEntity a where a not in (Select b FROM BoardEntity b where b.boardType = 3 order by b.like desc limit 3) order by a.createdDate desc")
+    List<BoardEntity> findRestArenaOrderByCreatedDateDesc();
 }
--- src/main/java/com/example/KeyboardArenaProject/service/arena/ArenaService.java
+++ src/main/java/com/example/KeyboardArenaProject/service/arena/ArenaService.java
@@ -3,7 +3,10 @@ package com.example.KeyboardArenaProject.service.arena;
 import com.example.KeyboardArenaProject.entity.BoardEntity;
 import com.example.KeyboardArenaProject.repository.BoardRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -17,5 +20,43 @@ public class ArenaService {
     public List<BoardEntity> findAllRankArena(){
         return boardRepository.findAllByBoardType(2);
     }
+
+    public List<BoardEntity> findTop3ArenaOrderByLikes(){
+        return boardRepository.findTop3ArenaByOrderByLikeDesc();
+    }
+
+    public List<BoardEntity> findRestArenaOrderByDate(){
+        return boardRepository.findRestArenaOrderByCreatedDateDesc();
+    }
+
+    /*
+    @Transactional
+    public void saveArena(String id,
+                          String boardId,
+                          String title,
+                          String content,
+                          int boardType,
+                          LocalDateTime created_date,
+                          LocalDateTime updated_date,
+                          int boardRank,
+                          int like,
+                          Boolean active,
+                          int view,
+                          int comment
+                          ){
+        BoardEntity board = new BoardEntity(id, boardId,
+                                            title,
+                                            content,
+                                            boardType,
+                                            created_date,
+                                            updated_date,
+                                            boardRank,
+                                            like,
+                                            active,
+                                            view,
+                                            comment);
+        boardRepository.save(board);
+    }
+     */
 
 }
