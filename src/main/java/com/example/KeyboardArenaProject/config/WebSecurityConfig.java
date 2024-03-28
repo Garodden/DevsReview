880a45e1cc8ed649f5f5e8208ef4b3ab4e29ab64
Hanbit <ghiesf@naver.com>
2024-03-28 09:19:23 +0900

    Feat: id찾기, pw찾기 api 진행중

--- src/main/java/com/example/KeyboardArenaProject/config/WebSecurityConfig.java
+++ src/main/java/com/example/KeyboardArenaProject/config/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.authorizeHttpRequests(auth ->
-				auth.requestMatchers("/login", "/signup", "/user")
+				auth.requestMatchers("/login", "/signup", "/user", "/findId", "/findPw")
 					.permitAll()
 					.anyRequest()
 					.authenticated())
