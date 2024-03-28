13fc95d0bed2df0761cb801726155e68f3b14435
YH <jsm1252@gmail.com>
2024-03-28 16:29:19 +0900

    Feat: 마이페이지 유저정보 조회, index.html 마이페이지 버튼추가

--- src/main/java/com/example/KeyboardArenaProject/service/user/MyPageService.java
+++ src/main/java/com/example/KeyboardArenaProject/service/user/MyPageService.java
@@ -1,7 +1,11 @@
 package com.example.KeyboardArenaProject.service.user;
 
+import com.example.KeyboardArenaProject.dto.user.MyPageInformation;
+import com.example.KeyboardArenaProject.entity.User;
 import com.example.KeyboardArenaProject.repository.MyPageRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MyPageService {
@@ -9,6 +13,23 @@ public class MyPageService {
 
     public MyPageService(MyPageRepository myPageRepository) {
         this.myPageRepository = myPageRepository;
+    }
+
+    public MyPageInformation getUserInfo(String userId) {
+        Optional<User> userOptional = myPageRepository.findByUserId(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            return new MyPageInformation(user);
+//            return MyPageInformation.builder()
+//                    .userId(user.getUserId())
+//                    .nickname(user.getNickname())
+//                    .userRank(user.getUserRank())
+//                    .point(user.getPoint())
+//                    .email(user.getEmail())
+//                    .build();
+        } else {
+            return null;
+        }
     }
 
 }
