b098951a1aa7bf4f0d329b192587cd8e907b18f4
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-03-31 15:37:37 +0900

    Fix/ ArenaController : showArenaDetails() 함수에서 유저가 해당 보드에 처음으로 도전하는지, 재도전하는지 확인하는 로직 추가.

--- src/main/java/com/example/KeyboardArenaProject/controller/arena/ArenaController.java
+++ src/main/java/com/example/KeyboardArenaProject/controller/arena/ArenaController.java
@@ -3,10 +3,12 @@ package com.example.KeyboardArenaProject.controller.arena;
 import com.example.KeyboardArenaProject.dto.arena.*;
 import com.example.KeyboardArenaProject.dto.user.UserTopBarInfo;
 import com.example.KeyboardArenaProject.entity.Board;
+import com.example.KeyboardArenaProject.entity.Cleared;
 import com.example.KeyboardArenaProject.entity.User;
+import com.example.KeyboardArenaProject.entity.compositeKey.UserBoardCompositeKey;
 import com.example.KeyboardArenaProject.service.CommentService;
 import com.example.KeyboardArenaProject.service.arena.ArenaService;
-import com.example.KeyboardArenaProject.service.user.CleaerdService;
+import com.example.KeyboardArenaProject.service.user.ClearedService;
 import com.example.KeyboardArenaProject.service.user.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -18,18 +20,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Tag(name = "아레나 CRUD")
 @Controller
 public class ArenaController {
     private final ArenaService arenaService;
-    private final CleaerdService cleaerdService;
+    private final ClearedService cleardService;
     private final CommentService commentService;
     private final UserService userService;
-    public ArenaController(ArenaService arenaService, CleaerdService cleaerdService, CommentService commentService, UserService userService){
+    public ArenaController(ArenaService arenaService, ClearedService cleardService, CommentService commentService, UserService userService){
         this.arenaService = arenaService;
-        this.cleaerdService = cleaerdService;
+        this.cleardService = cleardService;
         this.commentService =commentService;
         this.userService = userService;
     }
@@ -57,11 +60,9 @@ public class ArenaController {
     @Operation(summary = "개별 아레나 확인", description = " 유저 탑 네비게이션바, 아레나 정보, 아레나에 적힌 댓글들을 가져오는 API")
     @GetMapping("/arenas/{boardId}")
     public String showArenaDetails(@PathVariable String boardId, Model model) throws JsonProcessingException {
+
         Board arenaRawInfo = arenaService.findByBoardId(boardId);
 
-        String id = arenaRawInfo.getId();
-
-        //임시유저. 현재 로그인돼있는 기존 유저를 사용하면 됨.
         User user = userService.getCurrentUserInfo();
 
         ArenaDetailResponse arenaDetails = ArenaDetailResponse
@@ -69,10 +70,28 @@ public class ArenaController {
                 .user(user)
                 .board(arenaRawInfo)
                 .comment(commentService.findCommentsByBoardId(boardId))
-                .participates(cleaerdService.findParticipatesByBoardId(boardId))
+                .participates(cleardService.findParticipatesByBoardId(boardId))
                 .writer(userService.getNickNameById(arenaRawInfo.getId()))
                 .build();
         model.addAttribute("arena", arenaDetails);
+
+        UserBoardCompositeKey curUsersClearRecord = new UserBoardCompositeKey(user.getId(), boardId);
+
+        //클리어 보드에 현재 시작한 시간을 기록
+        //만약 클리어 기록이 존재하면 해당 기록에서 시간 기록 시작
+        //아니라면 새로운 기록 생성, 시간 기록 시작
+        if(cleardService.findIfUserClearDataExists(curUsersClearRecord)){
+            cleardService.update(curUsersClearRecord);
+        }
+        else {
+            Cleared cleared = Cleared.builder()
+                    .boardId(boardId)
+                    .id(user.getId())
+                    .startTime(LocalDateTime.now())
+                    .build();
+            cleardService.saveClearStartTime(cleared);
+        }
+
         return "arenaDetail";
     }
 
@@ -81,7 +100,7 @@ public class ArenaController {
     public String typeNewArena(Model model) {
         User currentUser = userService.getCurrentUserInfo();
         model.addAttribute("userTopBarInfo", new UserTopBarInfo(currentUser));
-    return "newArena";
+        return "newArena";
     }
 
     @Operation(summary = "아레나 제작", description = "아레나 개장 Post 메소드 API")
