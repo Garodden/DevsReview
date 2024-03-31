caa8778f0a2a564141f8014c4e530877215c58df
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-03-31 17:19:36 +0900

    Add/ 아레나 참전 후 결과를 알려주기 위한 response 객체 ArenaResultResponse 생성

---
+++ src/main/java/com/example/KeyboardArenaProject/dto/arena/ArenaResultResponse.java
@@ -0,0 +1,41 @@
+package com.example.KeyboardArenaProject.dto.arena;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ArenaResultResponse {
+    String title;
+    LocalTime time;
+    Integer participants;
+    Integer nthPlace;
+    Double percentage;
+
+    @Builder ArenaResultResponse(String title, int participants, LocalTime time, int nthPlace) {
+        this.title = title;
+        this.time = time;
+        this.participants = participants;
+        this.nthPlace = nthPlace;
+        this.percentage = (double) nthPlace / participants;
+    }
+
+    public String resultPopupText(Boolean ifCleared){
+        if(ifCleared) {
+            return "축하합니다 !\n" +
+                    title + " 아레나를" +
+                    time.toString() + "에 클리어 하셨군요!\n" +
+                    participants.toString() + "명 중 "+nthPlace.toString()+"+ 등으로\n" +
+                    "상위 " +percentage+"%입니다!";
+        }
+        else{
+            return title + " 아레나 클리어에 실패했습니다. 다시 시도해보세요!";
+        }
+    }
+}
