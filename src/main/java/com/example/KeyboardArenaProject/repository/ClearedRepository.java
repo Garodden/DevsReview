9b064b59da39f59bc538d19438db4fde82ccdde5
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-04-02 01:06:09 +0900

    FIX: ClearedRepository에서 ifClearedById가 클리어타임이 null인지에 대해 리턴하도록 수정

--- src/main/java/com/example/KeyboardArenaProject/repository/ClearedRepository.java
+++ src/main/java/com/example/KeyboardArenaProject/repository/ClearedRepository.java
@@ -13,8 +13,11 @@ import java.util.List;
 public interface ClearedRepository extends JpaRepository<Cleared, UserBoardCompositeKey> {
     List<Cleared> findAllByCompositeId_BoardId(String boardId);
 
+    //클리어 데이터가 있는지
     @Query("SELECT CASE WHEN e.clearTime is NULL then false ELSE true END FROM Cleared  e where e.compositeId =:id")
     Boolean ifClearedById(UserBoardCompositeKey id);
+
+    //유저 데이터가 있는지
 
     Long countByCompositeId_BoardId(String boardId);
 
