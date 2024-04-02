389adb48a720cc4f1ab41e86251bac3c4176582a
Chanhyeok Song <kacu0417@naver.com>
2024-04-02 15:50:47 +0900

    HotFix: 댓글 삭제 cascade 수정

--- src/main/java/com/example/KeyboardArenaProject/controller/freeBoard/FreeBoardController.java
+++ src/main/java/com/example/KeyboardArenaProject/controller/freeBoard/FreeBoardController.java
@@ -183,10 +183,9 @@ public class FreeBoardController {
     }
 
     @DeleteMapping("/comments/{comment_id}")
-    public String deleteComment(@PathVariable String comment_id){
+    public void deleteComment(@PathVariable String comment_id){
         String boardId = commentService.findCommentById(comment_id).getBoardId();
         commentService.deleteComment(comment_id);
-        return "redirect:/board/"+boardId;
     }
 
 }
