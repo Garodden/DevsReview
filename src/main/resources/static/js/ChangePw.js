4ad711897f082dba27ea097d775625a716be4128
YH <jsm1252@gmail.com>
2024-04-02 15:35:30 +0900

    Fix: (마이페이지)비밀번호 변경 api, ChangePw.js 코드 수정(주석제거, 비밀번호 변경 성공 시 logout)

--- src/main/resources/static/js/ChangePw.js
+++ src/main/resources/static/js/ChangePw.js
@@ -18,16 +18,13 @@ document.getElementById("changePasswordForm").addEventListener("submit", functio
         }
     }).then(function (response) {
         if (response.ok) {
-            // 비밀번호가 성공적으로 변경되었을 때
             alert("비밀번호가 성공적으로 변경되었습니다.");
-            window.location.href = "/mypage"; // 성공적으로 변경되었을 때 /mypage로 리다이렉트
+            window.location.href = "/logout";
         } else if (response.status === 400) {
             response.text().then(function (errorMessage) {
-                // 서버에서 반환한 오류 메시지를 받아와서 처리
                 alert(errorMessage);
             });
         } else {
-            // 기타 오류가 발생했을 때
             alert("서버와의 통신 중 문제가 발생했습니다. 나중에 다시 시도해주세요.");
         }
     });
