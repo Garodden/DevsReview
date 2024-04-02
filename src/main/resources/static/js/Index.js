128ffd700c0ca5ea83e59f45f81d75bcdee4ce75
Hanbit <ghiesf@naver.com>
2024-04-02 13:02:23 +0900

    Feat: 인덱스 페이지 아레나 리스트/자유게시판 리스트 전체조회 진행중

---
+++ src/main/resources/static/js/Index.js
@@ -0,0 +1,6 @@
+// const arenaListButton = document.querySelector("#arena-list-button");
+// const freeBoardListButton = document.querySelector("#freeboard-list-button");
+//
+// arenaListButton.addEventListener('click', (e) => {
+//     location.href='/arenas'
+// })
