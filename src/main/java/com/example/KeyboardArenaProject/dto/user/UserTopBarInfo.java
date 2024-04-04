2167f0b4845d50b31920f620a970e6c05c9fc413
Chanhyeok Song <kacu0417@naver.com>
2024-04-05 00:43:28 +0900

    Fix: 탈퇴유저 수정 불가능하게 하기 #51

--- src/main/java/com/example/KeyboardArenaProject/dto/user/UserTopBarInfo.java
+++ src/main/java/com/example/KeyboardArenaProject/dto/user/UserTopBarInfo.java
@@ -15,11 +15,13 @@ import lombok.Setter;
 public class UserTopBarInfo {
     String nickname;
     String id;
+    String userId;
     int rank;
     int point;
     public UserTopBarInfo(User user){
         this.nickname = user.getNickname();
         this.id = user.getId();
+        this.userId = user.getUserId();
         this.rank = user.getUserRank();
         this.point = user.getPoint();
     }
